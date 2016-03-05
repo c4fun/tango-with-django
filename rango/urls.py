626bdf08f9cb61ba35a44712f45e0221d2ab4ee6
c4fun <pcfeng502@gmail.com>
2016-03-05 10:23:25 +0700

    Change the base template to url tag
    - 1. Add a namespace for /rango in the project urls.py
    - 2. change the base template to url tag.
    - 3. Delete the /rango/ url mapping.

--- rango/urls.py
+++ rango/urls.py
@@ -3,7 +3,7 @@ from rango import views
 
 urlpatterns = [
     # name is optional--to allow you to distinguish one mapping from another
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
     url(r'^index/$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
